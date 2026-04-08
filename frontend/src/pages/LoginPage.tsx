d4431689252e033a8880c14a51d6abf0913b45f8
a1919727 <a1919727@adelaide.edu.au>
2026-04-08 22:03:46 +0930

    Add token in localStorage.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -58,6 +58,7 @@ export default function SignInSide() {
         },
       );
       console.log("Log in successfully", response.data);
+      localStorage.setItem("token", response.data.token);
       navigate("/Dashboard");
     } catch (error) {
       console.log("Failed to login", error);
