51cc6d9f38e99d44592fc379172b6cd2d7038c19
a1919727 <a1919727@adelaide.edu.au>
2026-04-12 19:30:03 +0930

    Remove unnescessary logout button on the header bar.

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -2,36 +2,16 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import { Drawer } from "@mui/material";
 import SideBar from "./SideBar";
 
 export default function HeaderBar() {
-  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
-  };
-
-  const handleLogout = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5001/api/auth/logout",
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        },
-      );
-      console.log("Log out successfully", response);
-      navigate("/login");
-    } catch (error) {
-      console.log("Failed to log out", error);
-    }
   };
 
   return (
@@ -67,13 +47,6 @@ export default function HeaderBar() {
           >
             Job Tracker
           </Typography>
-          <Button
-            color="inherit"
-            sx={{ fontWeight: "bold" }}
-            onClick={handleLogout}
-          >
-            Logout
-          </Button>
         </Toolbar>
       </AppBar>
     </Box>
