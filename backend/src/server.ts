6dc51be1b1211aeb479560bdfac685cfe18100a3
a1919727 <a1919727@adelaide.edu.au>
2026-04-13 08:03:06 +0930

    Bind backend server to 0.0.0.0 for Azure App Service.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,9 @@ import app from "./app";
 import { connectToDB } from "./config/mongoose";
 
 const port = Number(process.env.PORT) || 5000;
-const host = "127.0.0.1";
 
 connectToDB();
 
-app.listen(port, host, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
